a9598f18557de128cc2e350896510ca4bcf6bae2
Mark Levedahl <mlevedahl@gmail.com>
2026-05-20 16:24:00 -0400

    git-gui: guard set/unset of GIT_DIR and GIT_WORK_TREE
    git-gui unconditionally exports _gitdir as GIT_DIR, and _gitworktree as
    GIT_WORK_TREE, to the environment, and furthermore unconditionally
    unsets these environment variables in many
    
    git gui must have a repository, so _gitdir can never be empty and its
    export is always valid if repository discovery completes successfully.
    
    git gui might not find a worktree, so _gitworktree can be empty. While
    having no worktree is valid for blame/browser subcommands, exporting
    GIT_WORK_TREE=<empty> is not valid. Rather, an empty GIT_WORK_TREE
    raises errors in git builtins, for instance 'git branch --show-current'
    as used by git, and causes breakage. This is one cause of git blame /
    git browser not working without a worktree.
    
    A user may set GIT_DIR and/or GIT_WORK_TREE to override git's normal
    discovery rules, including repository configuration of core.worktree
    and/or worktree specific gitdirs. It is always safe to export the
    absolute pathnames of the discovered values, even though they may not be
    needed. However, the gitdir might not be found from the worktree without
    GIT_DIR being set. Furthermore, the worktree defined by the discovered
    gitdir might be overridden by GIT_WORK_TREE set before git-gui started.
    So, it is also sometimes necessary that one or both of these variables
    is set.
    
    So, let's provide two procs, one to unset GIT_DIR / GIT_WORK_TREE if
    they are set, one to set GIT_DIR and, if not empty, GIT_WORK_TREE,  so
    all call sites do the same thing, and problems with _gitworktree == {}
    are avoided.
    
    Signed-off-by: Mark Levedahl <mlevedahl@gmail.com>
    Signed-off-by: Johannes Sixt <j6t@kdbg.org>

--- git-gui.sh
+++ git-gui.sh
@@ -1122,6 +1122,22 @@ unset argv0dir
 ##
 ## repository setup
 
+proc set_gitdir_vars {} {
+	global _gitdir _gitworktree env
+	if {$_gitdir ne {}} {
+		set env(GIT_DIR) $_gitdir
+	}
+	if {$_gitworktree ne {}} {
+		set env(GIT_WORK_TREE) $_gitworktree
+	}
+}
+
+proc unset_gitdir_vars {} {
+	global env
+	catch {unset env(GIT_DIR)}
+	catch {unset env(GIT_WORK_TREE)}
+}
+
 set picked 0
 if {[catch {
 		set _gitdir $env(GIT_DIR)
@@ -1207,8 +1223,8 @@ if {[lindex $_reponame end] eq {.git}} {
 	set _reponame [lindex $_reponame end]
 }
 
-set env(GIT_DIR) $_gitdir
-set env(GIT_WORK_TREE) $_gitworktree
+# Export the final paths
+set_gitdir_vars
 
 ######################################################################
 ##
@@ -2007,7 +2023,7 @@ proc incr_font_size {font {amt 1}} {
 
 proc do_gitk {revs {is_submodule false}} {
 	global current_diff_path file_states current_diff_side ui_index
-	global _gitdir _gitworktree
+	global _gitworktree
 
 	# -- Always start gitk through whatever we were loaded with.  This
 	#    lets us bypass using shell process on Windows systems.
@@ -2017,8 +2033,6 @@ proc do_gitk {revs {is_submodule false}} {
 	if {$exe eq {}} {
 		error_popup [mc "Couldn't find gitk in PATH"]
 	} else {
-		global env
-
 		set pwd [pwd]
 
 		if {!$is_submodule} {
@@ -2050,13 +2064,11 @@ proc do_gitk {revs {is_submodule false}} {
 			# TODO we could make life easier (start up faster?) for gitk
 			# by setting these to the appropriate values to allow gitk
 			# to skip the heuristics to find their proper value
-			unset env(GIT_DIR)
-			unset env(GIT_WORK_TREE)
+			unset_gitdir_vars
 		}
 		safe_exec_bg [concat $cmd $revs "--" "--"]
 
-		set env(GIT_DIR) $_gitdir
-		set env(GIT_WORK_TREE) $_gitworktree
+		set_gitdir_vars
 		cd $pwd
 
 		if {[info exists main_status]} {
@@ -2079,21 +2091,16 @@ proc do_git_gui {} {
 	if {$exe eq {}} {
 		error_popup [mc "Couldn't find git gui in PATH"]
 	} else {
-		global env
-		global _gitdir _gitworktree
-
 		# see note in do_gitk about unsetting these vars when
 		# running tools in a submodule
-		unset env(GIT_DIR)
-		unset env(GIT_WORK_TREE)
+		unset_gitdir_vars
 
 		set pwd [pwd]
 		cd $current_diff_path
 
 		safe_exec_bg [concat $exe gui]
 
-		set env(GIT_DIR) $_gitdir
-		set env(GIT_WORK_TREE) $_gitworktree
+		set_gitdir_vars
 		cd $pwd
 
 		set status_operation [$::main_status \
